096bcef73bc589c646a086eb36b828e11679663a
Yassir RAMDANI <yassir.ramdani@epita.fr>
2020-07-29 06:17:17 +0200

    testsuite works on osx and linux64 now

--- tests/testsuite.sh
+++ tests/testsuite.sh
@@ -4,17 +4,31 @@ RED='\033[0;41m'
 NC='\033[0m' # No Color
 GREEN='\033[0;42m'
 
+echo "Please choose your os: [osx / linux64]"
+read choosen_os
+
+if [ $choosen_os = "osx" ]
+then
+	path_to_ref="ref/osx"
+elif [ $choosen_os = "linux64" ]
+then
+	path_to_ref="ref/linux64"
+else
+	echo "Unavailable choice!"
+	exit 1
+fi
+
 echo "[INFO] Compiling words.txt..."
-../TextMiningCompiler ../words.txt ../dict.bin
+"../TextMiningCompiler" ../words.txt ../dict.bin
 
 echo "[INFO] REF compiling words.txt..."
-../ref/TextMiningCompiler ../words.txt ../ref_dict.bin > /dev/null
+"../${path_to_ref}/TextMiningCompiler" ../words.txt ../ref_dict.bin > /dev/null
 
 for test_file in ../tests/test_files/*
 do
 	cat $test_file
-	cat $test_file | ../TextMiningApp ../dict.bin > .f1
-	cat $test_file | ../ref/TextMiningApp ../ref_dict.bin > .ref 2> /dev/null
+	cat $test_file | "../TextMiningApp" ../dict.bin > .f1
+	cat $test_file | "../${path_to_ref}/TextMiningApp" ../ref_dict.bin > .ref 2> /dev/null
 	diffval=$(diff -q .f1 .ref)
 	if [ "$diffval" = "" ] 
 	then
