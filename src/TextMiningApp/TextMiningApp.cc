c87c341a9a2e3789650ea126ef4a529a129dbe80
Yassir RAMDANI <yassir.ramdani@epita.fr>
2020-07-29 02:28:23 +0200

    fixed main TextMiningApp

--- src/TextMiningApp/TextMiningApp.cc
+++ src/TextMiningApp/TextMiningApp.cc
@@ -12,19 +12,13 @@ int main(int argc, char const *argv[])
 
     std::ifstream is(argv[1], std::ios::binary);
     std::string request;
+    int distance;
     std::cin >> request;
-    while(request == "approx" && is.is_open())
-    {
-        std::cin >> request;
-        int distance;
-        std::istringstream iss(request);
-        iss >> distance;
-        if (iss.fail())
-            return 0;
+    while(request == "approx" && is.is_open()) {
+        std::cin >> distance;
         std::cin >> request;
         approx_search(is, request, distance);
         is.seekg(0);
-
         std::cin >> request;
     }
 
